28417da61763990d5cc5a73f62cb2e0bb3f9eebb
Joaquim Cassano <joaquimcassano@gmail.com>
2024-12-02 21:25:53 -0300

    Added docstring on __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,22 @@
+"""
+The python SDK for the AbacatePay API
+
+Basic usage:
+```python
+import abacatepay
+
+token = "<your api token>"
+client = AbacatePay(token)
+
+billing = x = client.CreateBilling(products=[Product(externalId="123", name="Teste", quantity=1, price=101, description="Teste")], returnURL="https://abacatepay.com", completionUrl="https://abacatepay.com")
+print(billing.data.url)
+# > https://abacatepay.com/pay/aaaaaaa
+```
+
+More examples found on https://abacatepay.readme.io/
+"""
+
+
 from .src.billing import Billing
 from typing import Literal
 from ._models import Product, IBilling
