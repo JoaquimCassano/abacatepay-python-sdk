4bb2c327dfef319ff2b0a7b2342ffd12ee012739
Joaquim Cassano <joaquimcassano@gmail.com>
2024-12-04 17:40:29 -0300

    minor changes

--- abacatepay/tests/test_auth.py
+++ abacatepay/tests/test_auth.py
@@ -1,14 +1,7 @@
 import abacatepay
 import abacatepay._exceptions
 
-def test_WrongKey_onInit():
-  wrongKey = "Bearer abc1234"
-  try:
-    client = abacatepay.AbacatePay(wrongKey)
-    ErrObj = client
-  except Exception as e:
-    ErrObj = e
-  assert type(ErrObj) == abacatepay._exceptions.ForbiddenRequest
+
 
 def test_WrongKey_RunningFunction():
   rightKey = "Bearer 123456789"
